1d6bf11040d1972c1d929229a4eed7263d6ecfc8
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-23 15:54:32 +0200

    Add string methods

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const numberZero = {} ; //new Number(0);
+const numberZero = {}; //new Number(0);
 
-if(0) {
-  console.log(number)
+if (0) {
+    console.log(number);
 }
 
 const number = new Number(10);
@@ -19,7 +19,40 @@ new Number(num);
 console.log(num.toPrecision(7));
 console.log(num.toFixed(2));
 
+const numberTest = new Number(30.43143434);
+console.log(numberTest.toFixed(2));
+console.log("toString", numberTest.toString());
 
-const numberTest = new Number (30.43143434);
-console.log(numberTest.toFixed(2))
-console.log("toString",numberTest.toString())
+const str1 = "fdf";
+const strObj = new String("fdffdsf");
+
+if (str1) {
+    console.log(str1[1]);
+}
+
+if (strObj) {
+    console.log(strObj[2]);
+}
+
+if (strObj) {
+    console.log(strObj);
+}
+
+// toUpperCase
+const userName = "   tom   ";
+
+// console.log(userName.toUpperCase());
+const userNameUpper = userName.trim().toUpperCase().repeat(3);
+
+const arr = [1, 2, 3, 4, 5];
+const strArr = arr.join("-");
+console.log(strArr);
+
+const p =
+    "The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?";
+
+if (p.includes(".")) {
+    const posDote = p.indexOf(".");
+    const pArr = p.substring(0,posDote).split(" ");
+    console.log(pArr);
+}
